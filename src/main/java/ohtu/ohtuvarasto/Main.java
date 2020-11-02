3cf7845c53b672b59573b5c983b7c2811ce287f8
Pieta Tuomisto <pieta.tuomisto@helsinki.fi>
2020-11-02 14:46:26 +0200

    11.2

--- src/main/java/ohtu/ohtuvarasto/Main.java
+++ src/main/java/ohtu/ohtuvarasto/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
+        System.out.println("Varastosta päivää.")
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
