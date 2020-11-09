b6f124418c1aaf9dbde711893a0e28f372546814
Pieta Tuomisto <pieta.tuomisto@helsinki.fi>
2020-11-09 17:49:50 +0200

    2.3.4

--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -12,8 +12,5 @@ public class Main {
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < 1; j++) System.out.println("tämä rikkoo checkstylea");
-        }
     }
 }
